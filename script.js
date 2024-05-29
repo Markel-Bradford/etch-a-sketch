89ab334fef3a862abebcaab63253dc591d05c732
markel-bradford <markel.bradford94@gmail.com>
2024-05-29 18:19:35 -0500

    Adjusted makeGrid function to take one number and create a grid. Fixed flex properties so grid items adjust within thier container.

--- script.js
+++ script.js
@@ -3,22 +3,29 @@ window.onload = function() {
 const container = document.getElementById('container')
 let newGridBtn = document.getElementById('newGridBtn')
 
-function makeRows(rows, cols) {
-    // Clear existing grid when makeRows is called
+function makeGrid(cells) {
+    // Clear existing grid when makeGrid is called
     container.innerHTML = " "
-    
-    // Create css variables to dynamically set the number of rows and columns
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
 
-    // create a loop to create new divs based on the numbers given
-    for (i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement("div")
-        cell.innerText = " "
-        container.appendChild(cell).className = "grid-item"
-        // Create an event listen to color each grid item on hover
-        // Imitates the trail left on an etch-a-sketch
-        cell.addEventListener('mouseover', e => e.target.classList.add('hoverColor'))
+    // Create a loop to create new divs based on the numbers given
+    for (let i = 0; i < cells; i++) {
+        let row = document.createElement("div");
+        row.className = "row";        
+        if (i === 0) row.classList.add('first-row');
+        if (i === cells - 1) row.classList.add('last-row');
+        container.appendChild(row);
+
+        for (let c = 0; c < cells; c++) {
+            let col = document.createElement("div");
+            col.className = "col";
+            if (c === 0) col.classList.add('first-col');
+            if (c === cells - 1) col.classList.add('last-col');
+            row.appendChild(col);
+            
+            // Create an event listener to color each grid item on hover
+            // Imitates the trail left on an etch-a-sketch
+            col.addEventListener('mouseover', e => e.target.classList.add('hoverColor'));
+        }
     }
 }
 
@@ -26,24 +33,22 @@ function newGrid() {
         
 
         // Prompt the user to enter the desired number of rows and columns
-        let rows = prompt("Enter number of rows: ")
-        let cols = prompt("Enter number of columns: ")
+        let dimensions = prompt("Enter board dimensions: ")
 
-        rows = parseInt(rows)
-        cols = parseInt(cols)
+        let cells = parseInt(dimensions)
 
         // Create conditional statement that will require an input between 1 and 100
-        if (isNaN(rows) || isNaN(cols) || 100 >= rows <= 0 || 100 >= cols <= 0) {
+        if (isNaN(cells) || cells > 100 || cells < 1 ) {
             alert("Please enter numbers between 1 and 100 rows and columns.");
             return;
         }
         
-        makeRows(rows, cols) 
+        makeGrid(cells) 
     
     }
     newGridBtn.onclick = newGrid
 
     // Initial grid state
-    makeRows(16,16)
+    makeGrid(16)
 }
 
